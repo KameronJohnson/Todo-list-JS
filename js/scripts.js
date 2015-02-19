0677b8b7781f367f51d08fce21e6fd8672f0b437
kameron johnson & tucker <student@epicodus.com>
2015-02-19 15:02:23 -0800

    refactor logic, add formatting

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,12 @@ $(document).ready(function() {
     var inputtedListName = $("input#new-list-name").val();
     var newList = { listName: inputtedListName, tasks: [] }
 
+    $("ul#lists").append("<li><span class='list-name'>" + newList.listName + "</span></li>");
+      $(".list-name").last().click(function() {
+        $("#show-lists").show();
 
-
-
-  $("ul#lists").append("<li><span class='list-name'>" + newList.listName + "</span></li>");
-
-  $(".list-name").last().click(function() {
-    $("#show-lists").show();
-
-    $("#show-lists h2").text(newList.listName);
-  });
-    // $("ul#tasks").text("");
+        $("#show-lists h4").text(newList.listName);
+    });
 
 
     $("form#add-new-task").submit(function(event) {
@@ -27,17 +22,21 @@ $(document).ready(function() {
       $(".new-task").each(function() {
         var newTask = { taskName: inputtedTaskName }
         newList.tasks.push(newTask);
+      });
 
-    });
 
+      $("input#new-list-name").val("");
+      $("input#add-new-task").val("");
 
-    newList.tasks.forEach(function(task) {
-      $("ul#tasks").append("<li><span class='update-task'>" + task.taskName + "</li>");
-        $(".update-task").last().click(function() {
-          $("#show-tasks").show();
-        });
+      $("ul#tasks").text("");
+
+      newList.tasks.forEach(function(task) {
+        $("ul#tasks").append("<li><span class='update-task'>" + task.taskName + "</li>");
+          $(".update-task").last().click(function() {
+            $("#show-tasks").show();
+          });
       });
     });
-  });
 
-}); // document.ready
+  });
+});
